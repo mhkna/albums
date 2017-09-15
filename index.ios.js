0e6ecbd3d22fc3da457a30c0a7ce5e0494e83aff
Michael <michkna3@gmail.com>
2017-09-15 13:24:58 -0500

    header component added with some initial styling and use of flexbox

--- index.ios.js
+++ index.ios.js
@@ -1,15 +1,15 @@
 // import library to help create component
 
 import React from 'react';
-//import textproperty from library
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header.js';
 
 
 // create a component
 
 const App = () => {
   return (
-    <Text>Some Text</Text>
+    <Header />
   );
 };
 
