f3229bc960e33cf6d6109478043a56056429eeef
Michael <michkna3@gmail.com>
2017-10-02 17:17:11 -0500

    added beginning of button component to card"

--- index.ios.js
+++ index.ios.js
@@ -11,7 +11,7 @@ import AlbumList from './src/components/AlbumList.js';
 const App = () => {
   return (
     //add view tag because cant return two children in one function otherwise
-    <View>
+    <View style={{ flex: 1 }}>
       <Header headerText={'Albums'} />
       <AlbumList />
     </View>
